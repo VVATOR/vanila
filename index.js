e163d2549880e1a6109cec0e4f2c667bcc68c8b3
Vlad <Vlad@mail.ru>
2018-04-25 01:11:05 +0300

    png

--- index.js
+++ index.js
@@ -53,10 +53,21 @@ const lifecycle = (() => {
         console.log("create");
 
         factoryMove.fromTopToDown();
+        factoryMove.fromRightToLeft();
     }
 
     start = () => {
         console.log("start");
+
+
+        let idInterval = setInterval(() => {
+
+            lifecycle.create();
+            factoryMove.fromTopToDown();
+            factoryMove.fromRightToLeft();
+
+        }, 1000);
+        
     }
 
     stop = () => {
@@ -81,46 +92,103 @@ const lifecycle = (() => {
 })();
 
 
+
+
+
+
 const factoryMove = (() => {
 
     fromRightToLeft = () => {
         console.log("fromRightToLeft");
         // factoryA.getInstance(TYPE_CLOUD);
-        let list = [];
+        let list_bird = [];
 
-        list = [...document.querySelectorAll(".bird")];
+        list_bird = [...document.querySelectorAll(".bird")];
         setInterval(() => {
 
-            list.forEach(element => {
+           
+            list_bird.forEach(element => {
+                if(isCrash(element)){
+                    let offset = parseInt(element.style.left);
+                    if (offset > 0) {
+                        element.style.left = (offset - 10) + "px";
+                        // console.log(element.style.left + "  aa  " + offset);
+                    }
+                    // console.log(element.style.left + "  aa  " + offset);
+                }
+            })
+            
+        }, 90);
+
+
+        let list_cloud = [];
+        list_cloud = [...document.querySelectorAll(".cloud")];
+        setInterval(() => {
+            
+            list_cloud.forEach(element => {
 
                 let offset = parseInt(element.style.left);
-                element.style.left = (offset - 10) + "px";
+                if (offset > -200) {
+                    element.style.left = (offset - 1) + "px";
+                    // console.log(element.style.left + "  aa  " + offset);
+                }
                 // console.log(element.style.left + "  aa  " + offset);
             })
-        }, 50);
+        }, 10);
     };
 
+    
     fromTopToDown = () => {
         console.log("fromTopToDown");
         let list = [];
 
-        list = [...document.querySelectorAll(".bird")];
-        list.forEach(element => {
+        // list = [...document.querySelectorAll(".bird")];
+        // list.forEach(element => {
+        //     let idInterval = setInterval(() => {
+
+        //         let offset = parseInt(element.style.top);
+        //         if (offset < 768) {
+        //             element.style.top = (offset + 10) + "px";
+        //             // console.log(element.style.left + "  aa  " + offset);
+        //         }
+        //     }, getRandom(10, 1000));
+        //     /*  let offset = parseInt(element.style.top);
+        //       if (offset > 700) {
+        //           let b = document.getElementById("root");
+        //           b.removeChild(element);
+        //           clearInterval(idInterval);
+        //           console.log(element.style.left + "  aa  " + offset);
+        //       }*/
+        // });
+
+                    
+        let list_star = [];
+
+        list_star = [...document.querySelectorAll(".star")];
+        list_star.forEach(element => {
             let idInterval = setInterval(() => {
 
                 let offset = parseInt(element.style.top);
                 if (offset < 768) {
-                    element.style.top = (offset + 10) + "px";
+                    element.style.top = (offset + 1) + "px";
                     // console.log(element.style.left + "  aa  " + offset);
                 }
-            }, getRandom(10, 1000));
-            /*  let offset = parseInt(element.style.top);
-              if (offset > 700) {
-                  let b = document.getElementById("root");
-                  b.removeChild(element);
-                  clearInterval(idInterval);
-                  console.log(element.style.left + "  aa  " + offset);
-              }*/
+            }, 5);
+        });
+
+                            
+        let list_fuel = [];
+
+        list_fuel = [...document.querySelectorAll(".fuel")];
+        list_fuel.forEach(element => {
+            let idInterval = setInterval(() => {
+
+                let offset = parseInt(element.style.top);
+                if (offset < 768) {
+                    element.style.top = (offset + 1) + "px";
+                    // console.log(element.style.left + "  aa  " + offset);
+                }
+            }, 10);
         });
     };
 
@@ -152,4 +220,51 @@ const factoryMove = (() => {
 
 
 
+
+
+
+
+//////////////////////////////////
+function init(){
+   var plane = document.querySelector('#plane');
+    px1 = parseInt(plane.style.left);
+    px2 = parseInt(plane.style.left) +  parseInt(plane.offsetWidth);
+    py1 = parseInt(plane.style.top);
+    py2 = parseInt(plane.style.top)  +  parseInt(plane.offsetHeight);
+}
+
+var px1 =0;
+var px2 =0;
+var py1 =0;
+var py2 =0;
+
+
+function isCrash(element){
+    let ex1 = parseInt(element.style.left);
+    let ex2 = parseInt(element.style.left) +  parseInt(element.offsetWidth);
+    let ey1 = parseInt(element.style.top);
+    let ey2 = parseInt(element.style.top)  +  parseInt(element.offsetHeight);
+    console.log( ex1-px2 ) 
+   
+    return ( (parseInt(element.offsetWidth) + parseInt(plane.offsetWidth)) < ex2-px1  )
+    //&&
+    //( (parseInt(element.offsetHeight) + parseInt(element.style.top)) > (py1 + parseInt(plane.offsetHeight)) )
+    ;
+    /*if(ey1<py1<ey2 && ex1<px1<ex2){
+        console.log(ey1<py1<ey2 && ex1<px1<ex2)
+        return false;
+    }
+    console.log(ey1<py1<ey2 && ex1<px1<ex2)
+    return true;*/
+
+}
+
+
+function planeUp(){
+    plane.style.top = (parseInt(plane.style.top) - 20) +'px';
+}
+
+function planeDown(){
+    plane.style.top = (parseInt(plane.style.top) + 20) +'px';
+}
 
